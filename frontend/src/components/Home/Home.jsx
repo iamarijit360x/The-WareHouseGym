df6e4bca964849b15577aba5c8d8fba3c2982612
Arijit Banerjee <53684453+iamarijit360x@users.noreply.github.com>
2024-04-08 15:56:56 +0530

    Home Updated

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Home.css'; // Import your CSS file where you define background styles
-import image from '../../assets/back.png';
+import image from '../../assets/cover.png';
 
 export default function Home() {
   return (
     <Container fluid className="background-container">
-      <Row>
-        <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
-          <img src={image} height="100%" className="img-fluid" alt="Background" />
+      <Row >
+        <Col xs={12} md={6} className="d-flex align-items-center justify-content-center align-items-lg-center">
+          <img src={image} style={{maxHeight:"400px",overflow:"hidden"}} className="img-fluid" alt="Background" />
         </Col>
         <Col xs={12} md={6} className="d-flex align-items-center justify-content-center flex-column">
             <Row><h2 className="text-center">Strength, Sweat, Success</h2></Row>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Navbar, Nav,Container} from 'react-bootstrap';
-import logo from "../assets/gymlogo.png"
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from "../assets/gymlogo.png";
+
 export default function MyNavbar() {
   return (
-    <Navbar sticky="top" bg="body-tertiary" className="gap-5 px-4"  expand="sm" data-bs-theme="dark" position-fixed>
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={logo}
-          width="35"
-          height="auto"
-          className="d-inline-block align-top"
-        />{' '}
-        The Warehouse Gym
-    
-      </Navbar.Brand>
+    <Navbar sticky="top" bg="body-tertiary" className="px-4" expand="sm" data-bs-theme="dark" position-fixed>
+      <Container>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <img
+            alt=""
+            src={logo}
+            width="35"
+            height="auto"
+            className="d-inline-block align-top mr-2"
+          />
+          <span className="d-none d-md-inline">The Warehouse Gym</span>
+        </Navbar.Brand>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="flex-grow-1 justify-content-around">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="#">Price</Nav.Link>
-          <Nav.Link href="#">Contact Us</Nav.Link>
-          <Nav.Link href="#">Hello,Sign In</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="flex-grow-1 justify-content-around">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">Price</Nav.Link>
+            <Nav.Link href="#">Contact Us</Nav.Link>
+            <Nav.Link href="#">Hello, Sign In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
