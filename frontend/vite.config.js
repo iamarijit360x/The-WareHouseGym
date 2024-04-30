5e75179f51d91a1f80b16903d4a3beb49e1c0b0e
Arijit Banerjee <53684453+iamarijit360x@users.noreply.github.com>
2024-04-30 16:40:06 +0530

    Vite Config Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:'public'
+  build:'public',
+  server: {
+    // Set the MIME type for JavaScript files
+    mimeTypes: {
+      '.js': 'application/javascript',
+    }},
 })
