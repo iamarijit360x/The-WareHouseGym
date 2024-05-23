60960f4e2fdaef697b789b618bf2fd10ca19df0f
Arijit Banerjee <53684453+iamarijit360x@users.noreply.github.com>
2024-05-24 01:54:15 +0530

    Security fix

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,6 +2,6 @@ const {userAuthticated,isAdmin}=require('../controllers/Authmiddleware')
 const express=require('express')
 const router=express.Router()
 const {getUsers}=require('../controllers/adminController')
-router.get('/admin',getUsers)
+router.get('/admin',userAuthticated,isAdmin,getUsers)
 
 module.exports = router;
